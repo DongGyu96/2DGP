ca5bc3026ae5a2d0926ad69eaf191f6d35ae1b2c
DongGyu96 <crow4079@naver.com>
2016-11-21 23:24:08 +0900

    Refactoring + Ball Destory Animation

---
+++ BGM_Object.py
@@ -0,0 +1,9 @@
+from pico2d import *
+
+class BGM:
+    TITLE, STAGE1, STAGE2, STAGE3, STAGE4, STAGE5, STAGE6, STAGE7, STAGE8, INFINITY = 0,1,2,3,4,5,6,7,8,9
+    def __init__(self, stage):
+        if stage == BGM.TITLE:
+            self.bgm = load_music('title_bgm.mp3')
+        self.bgm.set_volume(64)
+        self.bgm.repeat_play()
